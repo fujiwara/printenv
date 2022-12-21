dd04ca34e6b6b1a4057944fb4b2b96cfd7b3fd70
fujiwara <fujiwara.shunichiro@gmail.com>
2022-12-21 02:32:42 +0000

    add -latency and -randomize flag

--- go.mod
+++ go.mod
@@ -1,5 +1,14 @@
 module github.com/fujiwara/printenv
 
-go 1.16
+go 1.18
 
-require github.com/fujiwara/ridge v0.6.1
+require (
+	github.com/fujiwara/ridge v0.6.1
+	github.com/mash/go-accesslog v1.3.0
+)
+
+require (
+	github.com/aws/aws-lambda-go v1.26.0 // indirect
+	github.com/pires/go-proxyproto v0.6.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- main.go
+++ main.go
@@ -4,30 +4,73 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"sort"
 	"strings"
+	"time"
 
 	"github.com/fujiwara/ridge"
+	accesslog "github.com/mash/go-accesslog"
 )
+
+type Latency struct {
+	duration  time.Duration
+	randomize bool
+}
+
+func (l *Latency) Sleep() {
+	if l.duration == 0 {
+		return
+	}
+	var s time.Duration
+	if l.randomize {
+		s = time.Duration(rand.NormFloat64() * float64(l.duration))
+	} else {
+		s = l.duration
+	}
+	time.Sleep(s)
+}
+
+type Logger struct {
+	enc *json.Encoder
+}
+
+func (l Logger) Log(record accesslog.LogRecord) {
+	l.enc.Encode(record)
+}
+
+var latency = &Latency{}
 
 func main() {
 	var port int
+	logger := Logger{enc: json.NewEncoder(os.Stdout)}
+
 	flag.IntVar(&port, "port", 8080, "port number")
+	flag.DurationVar(&latency.duration, "latency", 0, "average latency")
+	flag.BoolVar(&latency.randomize, "randomize", false, "randomize latency")
 	flag.VisitAll(func(f *flag.Flag) {
 		if s := os.Getenv(strings.ToUpper(f.Name)); s != "" {
 			f.Value.Set(s)
 		}
 	})
 	flag.Parse()
+	log.Println("port:", port)
+	log.Printf("latency: avg:%s randomize:%v", latency.duration, latency.randomize)
 
 	var mux = http.NewServeMux()
 	mux.HandleFunc("/", handlePrintenv)
-	ridge.Run(fmt.Sprintf(":%d", port), "/", mux)
+	ridge.Run(
+		fmt.Sprintf(":%d", port),
+		"/",
+		accesslog.NewLoggingHandler(mux, logger),
+	)
 }
 
 func handlePrintenv(w http.ResponseWriter, r *http.Request) {
+	latency.Sleep()
 	ac := r.Header.Get("Accept")
 	if strings.Contains(ac, "application/json") {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
